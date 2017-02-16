171f2d2bc291b36ccab303efd40fe835ab5ddddf
Gurdip Singh <gurdipsingh1990@gmail.com>
2017-02-15 17:04:05 -0800

    Changes to buttons

--- testCode.js
+++ testCode.js
@@ -3,86 +3,77 @@ let dealerBody = document.getElementById('dealerBodyDiv');
 let player = document.getElementById('player');
 let dealer = document.getElementById('dealer');
 let hiddenCard = document.getElementById('dealer');
-let allFetchedData = [];
+let fetchedData = [];
 let discardedCardArr = [];
-let playBtnCount = 0; //if count === 4 play button disabled until new hand
-let runningCardCount = 0; //method to deal cards to player/dealer
-let playerCount = 0; //total of player
-let dealerCount = 0; //total of dealer
-let runCount = 0;
-let cantHitNoMore = 0;
-let cardsAndValuesArray = [];
 let dealerHiddenCardValue = [];
-let cardImgAndValueArr = [];
+
+let dealCount = 0;
+let playerTotal = 0;
+let dealerTotal = 0;
+let hitButtonEnabled = true;
+let gameOver = false;
+
 
 
 let hitButton = document.getElementById('hit');
 hitButton.addEventListener('click', function(event) {
   event.preventDefault();
-
   runningCardCount = 0;
-  if (cantHitNoMore > 0) {
-    return;
-  }
-  else {
-    delegateRoles(allFetchedData);
-    checkForBlackjack();
-  }
-})
 
-
-let newHand = document.getElementById('play');
-newHand.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  if (dealerHiddenCardValue.length > 0) {
-    while (playerBody.firstChild && dealerBody.firstChild) {
-      playerBody.removeChild(playerBody.firstChild);
-      dealerBody.removeChild(dealerBody.firstChild);
-      resetAllCounters();
-      //  return;
-    }
+  if (hitButtonEnabled === true) {
+    dealNewCard(fetchedData, player);
   } else {
-    while (playBtnCount < 10) {
-      fetchCard()
-      playBtnCount++;
-    }
-  }
-})
-
-
-
-let dealCount = 0;
-let deal = document.getElementById('deal')
-deal.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  if (runCount > 0) {
     return;
   }
-  else {
-    while (dealCount < 4) {
-      runCount++;
-      delegateRoles(allFetchedData);
-      dealCount++;
-    }
-  }
 })
+
 
 
 let standButton = document.getElementById('stand');
 standButton.addEventListener('click', function(event) {
   event.preventDefault();
+  hitButtonEnabled = false;
   runningCardCount = 3;
-  cantHitNoMore = 1;
+  disableStandutton = 0;
 
-  if (runCount < 5) {
-    checkDealerTotal();
-    runCount++;
+  if (disableStandutton < 1) {
+    changeDealerTotal();
+    disableStandutton++;
   }
-  // checkDealerTotal();
   else {
     return;
+  }
+})
+
+let loadDeck = document.getElementById('play');
+loadDeck.addEventListener('click', function(event) {
+  event.preventDefault();
+  let fetchCardsCount = 0;
+    while (fetchCardsCount < 10) {
+      fetchCard()
+      fetchCardsCount++;
+    }
+})
+
+let newGame = document.getElementById('newGame')
+newGame.addEventListener('click', function(event) {
+  event.preventDefault();
+  // let dealCount = 0;
+  let runCount = 0;
+  if (runCount > 0) {
+    return;
+  }
+  else {
+    while (dealCount < 4) {
+      if (dealCount % 2 === 0) {
+        dealNewCard(fetchedData, player);
+        dealCount++;
+      } else {
+        dealNewCard(fetchedData, dealer);
+        dealCount++;
+      }
+      runCount++
+    }
   }
 })
 
@@ -90,7 +81,6 @@ standButton.addEventListener('click', function(event) {
 let refreshButton = document.getElementById('refresh');
 refreshButton.addEventListener('click', function(event) {
   window.location.reload();
-
 })
 
 
@@ -104,180 +94,185 @@ function fetchCard() {
     .then(function(jsonObj) {
       let cardsObj = jsonObj.cards;
       for (var i = 0; i < cardsObj.length; i++) {
-        allFetchedData.push({
+        fetchedData.push({
           imgAddress: cardsObj[i].image,
           cardValue: cardsObj[i].value
         })
       }
-      return Promise.all(allFetchedData);
+      return Promise.all(fetchedData);
     })
 }
 
 
 
-function delegateRoles(arr) {
+function dealNewCard(arr, dealTo) {
   let tempVar = '';
   for (var i = 0; i < 1; i++) {
-    assignCard(arr[i].imgAddress);
-    caluculateCount(arr[i].cardValue);
-    tempVar = arr.shift();
-    discardedCardArr.push(tempVar);
+    if (dealTo === player) {
+      assignCard(arr[i].imgAddress, player);
+      caluculateCount(arr[i].cardValue);
+      // updatePlayerGameStats(arr[i].cardValue);
+      tempVar = arr.shift();
+      discardedCardArr.push(tempVar);
+    } else if (dealTo === dealer) {
+      assignCard(arr[i].imgAddress, dealer);
+      caluculateCount(arr[i].cardValue);
+      // changeDealerTotal(arr[i].cardValue);
+      tempVar = arr.shift();
+      discardedCardArr.push(tempVar);
+    }
   }
-
-  runningCardCount++;
-  console.log('running card count in delegateRoles: '+runningCardCount);
 }
 
 
 //
-function assignCard(cardImgAdress) {
+function assignCard(cardImgAdress, dealTo) {
   let img = new Image();
   img.style.width = '170px';
   img.style.height = '220px';
-  // console.log(runningCardCount);
-  if (runningCardCount === 1) {
-    dealerHiddenCardValue.push(cardImgAdress);
-    img.src = './images/cheetah.jpg';
-    hiddenCard = img;
-    hiddenCard.style.float = 'left';
-    return dealerBody.appendChild(hiddenCard);
-  } else if (runningCardCount % 2 === 0) {
+
+  if (dealTo === dealer) {
+    if (dealCount === 1) {
+      dealerHiddenCardValue.push(cardImgAdress);
+      img.src = './images/cheetah.jpg';
+      hiddenCard = img;
+      hiddenCard.style.float = 'left';
+      return dealerBody.appendChild(hiddenCard);
+    }
+    else {
+      img.src = cardImgAdress.toString();
+      dealer = img;
+      dealer.style.float = 'left';
+      return dealerBody.appendChild(dealer);
+    }
+  }
+  else if (dealTo === player) {
     img.src = cardImgAdress.toString();
     player = img;
     player.style.float = 'left';
     return playerBody.appendChild(player);
-  } else {
-    img.src = cardImgAdress.toString();
-    dealer = img;
-    dealer.style.float = 'left';
-    return dealerBody.appendChild(dealer);
   }
+
 }
+
+
+
 
 
 function caluculateCount(cardVal) {
-  console.log('card value in caluculateCount: '+cardVal);
-  if ((runningCardCount === 1)) {
+  // console.log('card value in caluculateCount: '+cardVal);
+  if ((dealCount === 1)) {
     dealerHiddenCardValue.push(cardVal);
     cardVal = 0;
-    dealerCount += parseInt(cardVal);
+    dealerTotal += parseInt(cardVal);
+    document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
   }
 
-  else if (runningCardCount % 2 === 0) {
+  else if (dealCount % 2 === 0) {
     if (!parseInt(cardVal)) {
       if (cardVal === 'ACE') {
-        return valueOfAce(player, playerCount);
+        valueOfAce(cardVal, player);
+        document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerTotal;
+        return
       }
       cardVal = 10;
     }
-    playerCount += parseInt(cardVal);
-    document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerCount;
+    playerTotal += parseInt(cardVal);
+    document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerTotal;
   }
 
   else {
     if (!parseInt(cardVal)) {
       if (cardVal === 'ACE') {
-        return valueOfAce(cardVal, dealerCount);
+        valueOfAce(cardVal, dealer);
+        document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
+        return
       }
       cardVal = 10;
     }
-    dealerCount += parseInt(cardVal);
-    document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerCount;
+    dealerTotal += parseInt(cardVal);
+    document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
   }
 }
+
+
+
+
 
 
 function valueOfAce(value, countOn) {
-  if (countOn === playerCount) {
-    if (playerCount < 10) {
-      console.log('if player count is < 10');
+  if (countOn === player) {
+    if (playerTotal < 10) {
+      // console.log('if player count is < 10');
       value = 11;
-       playerCount += value;
-    } else if (playerCount > 10) {
-      console.log('if player count is > 10');
+      playerTotal += value;
+      document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerTotal;
+      return;
+    } else if (playerTotal > 10) {
+      // console.log('if player count is > 10');
       value = 1;
-       playerCount += value;
+      playerTotal += value;
+      document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerTotal;
+      return;
     }
-  } else if (countOn === dealerCount) {
-    if (dealerCount < 10) {
-      console.log('if dealer count is < 10');
+  }
+
+   else if (countOn === dealer) {
+    if (dealerTotal < 10) {
+      // console.log('if dealer count is < 10');
       value = 11;
-       dealerCount += value;
-    } else if (dealerCount > 10) {
-      console.log('if dealer count is > 10');
+      dealerTotal += value;
+      document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
+      return;
+    } else if (dealerTotal > 10) {
+      // console.log('if dealer count is > 10');
       value = 1;
-       dealerCount += value;
+      dealerTotal += value;
+      document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
+      return;
     }
   }
 }
 
-function checkForBlackjack() {
-  if (playerCount > 21) {
-    checkDealerTotal();
-  }
-  else if(playerCount === 21) {
-    checkDealerTotal();
-    }
-  else if (playerCount === dealerCount) {
-    alert('PUSH!');
-  }
-
-  else if(playerCount < 21) {
-    return;
-  }
-
-  else {
-    checkDealerTotal();
-  }
-}
 
 
-function checkDealerTotal() {
-  if (playerCount > 21) {
+
+
+
+
+function changeDealerTotal() {
+  if (playerTotal > 21) {
     changeHiddenCardImage();
     changeHiddenCardValue();
-    document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerCount;
+    document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
   }
   else {
-    while (dealerCount < 17) {
-      runningCardCount = 3;
+    while (dealerTotal < 17) {
+      dealNewCard(fetchedData)
       changeHiddenCardImage();
       changeHiddenCardValue();
-      delegateRoles(allFetchedData)
-      // checkForBlackjack();
+      document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerTotal;
+      // updatePlayerGameStats();
     }
   }
 }
 
 
 function changeHiddenCardImage() {
-  hiddenCard.src = discardedCardArr[0].imgAddress.toString();
+  hiddenCard.src = dealerHiddenCardValue[0].toString();
   hiddenCard.style.float = 'left';
   return dealerBody.appendChild(hiddenCard);
 }
 
 
 function changeHiddenCardValue() {
-  let tempVal = discardedCardArr[0].cardValue;
+  let tempVal = dealerHiddenCardValue[1].cardValue;
   if (!parseInt(tempVal)) {
     if (tempVal === 'ACE') {
-      return valueOfAce(tempVal, dealerCount);
+      return valueOfAce(tempVal, dealerTotal);
     }
     tempVal = 10;
   }
-  dealerCount += parseInt(tempVal);
-  return dealerCount;
+  dealerTotal += parseInt(tempVal);
+  return dealerTotal;
 }
-
-
-// function resetAllCounters() {
-//   document.getElementById("playerScore").innerHTML = 'Player Count: ' + playerCount;
-//   playerCount = 0;
-//   document.getElementById("dealerScore").innerHTML = 'Dealer Count: ' + dealerCount;
-//   dealerCount = 0;
-//   playBtnCount = 0;
-//   runningCardCount = 0;
-//   runCount = 0;
-//   cantHitNoMore = 0;
-//   //  return;
-// }
